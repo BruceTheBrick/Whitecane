780b40fd8d941662f37cd9bf5b31df1b97ba8300
Bryce Lynch <techbryce@hotmail.com>
2023-11-12 20:30:46 +1100

    REFACTOR - Modified WhiteCane.cs and PlatformInitializer for Android and IOS

--- Platforms/Android/PlatformInitializer.android.cs
+++ Platforms/Android/PlatformInitializer.android.cs
@@ -1,20 +1,17 @@
-﻿#if ANDROID
-using WhiteCane.Droid.Effects;
+﻿using WhiteCane.Droid.Effects;
 using WhiteCane.Effects;
 
 namespace WhiteCane.Platforms;
 
 internal static class PlatformInitializer
 {
-    internal static MauiAppBuilder InitializePlatform(this MauiAppBuilder builder)
+    internal static void InitializePlatform(this MauiAppBuilder builder)
     {
         builder.ConfigureEffects(RegisterPlatformEffects);
-        return builder;
     }
-
+    
     private static void RegisterPlatformEffects(IEffectsBuilder effects)
     {
         effects.Add(typeof(AccessibilityTraits), typeof(AccessibilityTraitsEffect));
     }
 }
-#endif
--- Platforms/iOS/PlatformInitializer.ios.cs
+++ Platforms/iOS/PlatformInitializer.ios.cs
@@ -1,19 +1,17 @@
-﻿#if IOS
-using WhiteCane.Effects;
+﻿using WhiteCane.Effects;
 using WhiteCane.iOS.Effects;
 
 namespace WhiteCane.Platforms;
 
 internal static class PlatformInitializer
 {
-    internal static MauiAppBuilder InitializePlatform(this MauiAppBuilder builder)
+    internal static void InitializePlatform(this MauiAppBuilder builder)
     {
         builder.ConfigureEffects(RegisterPlatformEffects);
-        return builder;
     }
+    
     private static void RegisterPlatformEffects(IEffectsBuilder effects)
     {
         effects.Add(typeof(AccessibilityTraits), typeof(AccessibilityTraitsEffect));
     }
 }
-#endif
