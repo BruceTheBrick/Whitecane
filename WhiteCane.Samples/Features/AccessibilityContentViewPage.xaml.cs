15b114e5e25dfa8e1e698a0c7292c1426529bda8
Bryce Lynch <techbryce@hotmail.com>
2023-12-29 00:18:20 +1100

    ADD - Added reference to WhiteCane from WhiteCane.Samples.csproj, added features to AccessibilityContentViewPage.xaml

--- WhiteCane.Samples/Features/AccessibilityContentViewPage.xaml.cs
+++ WhiteCane.Samples/Features/AccessibilityContentViewPage.xaml.cs
@@ -5,5 +5,19 @@ public partial class AccessibilityContentViewPage
     public AccessibilityContentViewPage()
     {
         InitializeComponent();
+        var incrementCommand = new Command(Increment);
+        var decrementCommand = new Command(Decrement);
+        AccessibilityContentView.IncrementCommand = incrementCommand;
+        AccessibilityContentView.DecrementCommand = decrementCommand;
+    }
+    
+    private void Increment()
+    {
+        SemanticScreenReader.Default.Announce("You have incremented!");
+    }
+
+    private void Decrement()
+    {
+        SemanticScreenReader.Default.Announce("You have decremented!");
     }
 }
