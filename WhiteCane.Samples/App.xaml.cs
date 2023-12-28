e338bf5b8d4e8a1fdb8733035c7fc26f31749764
BruceTheBrick <35167644+BruceTheBrick@users.noreply.github.com>
2023-12-29 00:00:47 +1100

    Introduce WhiteCane.Samples project (#1)
    * UPDATE - Updated project structure
    
    * ADD - Added WhiteCane.Samples project
    
    * UPDATE - Added structure to WhiteCane.Samples.csproj, introduced app icon and splash
    
    * UPDATE - Minor UI updates to HomePage.xaml, prevented Samples project from being packable

---
+++ WhiteCane.Samples/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WhiteCane.Samples;
+
+public partial class App
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ WhiteCane.Samples/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using WhiteCane.Samples.Features;
+
+namespace WhiteCane.Samples;
+
+public partial class AppShell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        RegisterRoutes();
+    }
+
+    private void RegisterRoutes()
+    {
+        Routing.RegisterRoute(Routes.HomePage, typeof(HomePage));
+        Routing.RegisterRoute(Routes.AccessibilityContentViewPage, typeof(AccessibilityContentViewPage));
+    }
+}
